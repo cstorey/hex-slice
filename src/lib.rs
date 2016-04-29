ef7cd1e660a13ce5bc32fec6d8f94a6151b8d702
Ceri Storey <cez@necrofish.org.uk>
2016-04-29 17:29:00 +0100

    Implement hexdump for slices.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,43 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::fmt;
+
+pub struct Hex<'a, T: 'a>(&'a [T]);
+
+pub trait AsHex {
+    type Item;
+    fn as_hex<'a>(&'a self) -> Hex<'a, Self::Item>;
+}
+
+impl<'a, T> Hex<'a, T> {
+    pub fn hex(slice: &'a [T]) -> Hex<'a, T> {
+        Hex(slice)
+    }
+}
+
+impl<'a, T: fmt::LowerHex> fmt::LowerHex for Hex<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        try!(write!(f, "["));
+        for (i, val) in self.0.iter().enumerate() {
+            try!(write!(f, "{}{:x}", if i > 0 { " " } else { "" }, val ))
+        }
+        try!(write!(f, "]"));
+        Ok(())
+    }
+}
+
+impl<'a, T: fmt::UpperHex> fmt::UpperHex for Hex<'a, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        try!(write!(f, "["));
+        for (i, val) in self.0.iter().enumerate() {
+            try!(write!(f, "{}{:X}", if i > 0 { " " } else { "" }, val ))
+        }
+        try!(write!(f, "]"));
+        Ok(())
+    }
+}
+
+impl<T> AsHex for [T] {
+    type Item = T;
+    fn as_hex<'a>(&'a self) -> Hex<'a, Self::Item> {
+        Hex::hex(self)
     }
 }
